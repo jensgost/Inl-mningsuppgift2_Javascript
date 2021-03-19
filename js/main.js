a65100cf5bc965dce41bdf603a18a125a27a8d97
Jens <jens.gost@gmail.com>
2021-03-19 10:00:52 +0100

    Continued working with old code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 "strict mode";
-// tar in value från search bar
-/*  
-let searchBar = document.querySelector("#csearch");
-searchBar.onchange = function(e){
-    alert(searchBar.value);
-}*/
 
-/*
+// definerar variabler för att kommma åt veckodag, temperatur, väder samt plats
+const locationElement = document.querySelector(".location p");
+const weekdayElement = document.querySelector(".weekday p");
+const tempValueElement = document.querySelector(".temperature p");
+const tempDescElement = document.querySelector(".condition p");
+
+
 function getCityWeatherURL(input) {
 
 const url = new URL("https://api.openweathermap.org/data/2.5/weather");
@@ -17,13 +17,30 @@ url.searchParams.set("mode", "json");
 url.searchParams.set("units", "metric");
 url.searchParams.set("lang", "se");
 
-outputElement.innerText = url;
-
 return url;
-}*/
+}
 
-// definerar variabler för att kommma åt veckodag, temperatur, väder samt plats
-const weekdayElement = document.querySelector(".weekday p");
-const tempValueElement = document.querySelector(".temperature-value p");
-const tempDescElement = document.querySelector(".temperature-description p");
-const locationElement = document.querySelector(".location p");
+async getWeatherData(input) {
+
+    const myKey = "f8f1da564369558f038f69a2bfa06360";
+
+   // make request to url
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+    );
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+    }
+
+function displayWeather(data){
+    weekdayElement.innerHTML = 
+}
+
+// tar in value från search bar
+/*  
+let searchBar = document.querySelector("#csearch");
+searchBar.onchange = function(e){
+    alert(searchBar.value);
+}*/
